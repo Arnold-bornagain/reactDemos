38cf8c277a3d5371284ecaaa043cf837b748ca1c
Kato Agaba <kagaba@innovationvillage.co.ug>
2021-11-25 17:13:01 +0300

    sample demonstarting the use of styled-components in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";//The Router, Switch, and Route will help us move between our created component and the main App.js.
+import Landing from "./components/Landing" //This is to import the component created in the Landing.js file.
+import Header from "./components/Header"  // This is to import the component created in the Header.js file.
+import './App.css';//Load a set of predefined CSS that will define how HTML elements in the landing page behave.
+ 
+function App() {   //main app
+   return(
+  
+   <Router>
+     <Routes>
+       <Route  path="/" element={<Landing />} />
+      
+       
+     </Routes>
+   </Router>
+  )};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ export default App; 
